d647753537b14029b10d0dad968b4653b68502a5
zhouyinyan <gruelac@126.com>
2015-06-24 15:50:21 +0800

    init

---
+++ javaTest-assemble/src/test/java/com/gztpay/test/bootstrap/JavaTestTomcatBootStrap.java
@@ -0,0 +1,14 @@
+/**
+ * created since 2012-5-23
+ */
+package com.gztpay.test.bootstrap;
+
+
+import com.gztpay.test.bootstrap.tomcat.TomcatBootstrapHelper;
+
+public class JavaTestTomcatBootStrap {
+ 
+	public static void main(String[] args) throws Exception {
+		new TomcatBootstrapHelper(8080,false, "test").start();
+	}
+}
