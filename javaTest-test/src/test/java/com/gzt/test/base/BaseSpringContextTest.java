6589452ffeae36b0e7c4965d8d96a6993b8e44f8
zhouyinyan <gruelac@126.com>
2015-06-25 12:15:48 +0800

    logback日志配置中在测试用例中不能正确读取环境变量，给个默认值。

--- javaTest-test/src/test/java/com/gzt/test/base/BaseSpringContextTest.java
+++ javaTest-test/src/test/java/com/gzt/test/base/BaseSpringContextTest.java
@@ -2,10 +2,13 @@ package com.gzt.test.base;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import static org.junit.Assert.*;
 /**
  * Created by zyinyan on 2015/6/23.
  */
@@ -13,17 +16,24 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration({"classpath*:spring/*.xml"})
 public class BaseSpringContextTest {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     static {
         System.setProperty("spring.profiles.active", "dev");
     }
 
     @Before
     public void setUp() throws Exception {
-
+        logger.info("begin test......");
     }
 
     @After
     public void tearDown() throws Exception {
+        logger.info("end test......");
+    }
 
+    @Test
+    public void testName() throws Exception {
+        assertTrue(true);
     }
 }
