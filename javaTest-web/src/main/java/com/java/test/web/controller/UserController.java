bc33f1a7a012b798502b02ad4883aafa88983d07
zhouyinyan <gruelac@126.com>
2015-06-26 15:10:21 +0800

    添加durid servlet 添加dubbo支持 添加dubbo demo（demoservice）

---
+++ javaTest-web/src/main/java/com/java/test/web/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.java.test.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zyinyan on 2015/6/25.
+ */
+@Controller
+public class UserController {
+}
