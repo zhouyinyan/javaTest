7d848849f27fa210567b55d12c53248d4fc40725
zhouyinyan <gruelac@126.com>
2015-06-27 14:14:33 +0800

    添加dubbo performance log的filter 和 测试demo

--- javaTest-biz/src/main/java/com/test/java/biz/api/impl/DemoServiceImpl.java
+++ javaTest-biz/src/main/java/com/test/java/biz/api/impl/DemoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.test.java.biz.api.impl;
 
+import com.test.java.common.constants.AppConstants;
 import com.test.java.facade.api.demoservice.DemoService;
 import com.test.java.facade.api.demoservice.DemoServiceOrder;
 import com.test.java.facade.api.demoservice.DemoServiceResult;
 import com.test.java.facade.api.demoservice.exceptions.DemoServiceException;
+import org.perf4j.StopWatch;
+import org.perf4j.slf4j.Slf4JStopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,14 +17,19 @@ public class DemoServiceImpl implements DemoService {
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
+    Logger performanceLogger = LoggerFactory.getLogger(AppConstants.PERFORMANCE_LOGGER);
+
     @Override
     public DemoServiceResult demo(DemoServiceOrder order) throws DemoServiceException {
+
+        StopWatch stopWatch = new Slf4JStopWatch("execute DemoServiceImpl.deme()",performanceLogger);
         logger.info("order:{}",order);
 
         DemoServiceResult result = new DemoServiceResult();
         result.setOrderNo(order.getOrderNo());
 
         logger.info("result:{}", result);
+        stopWatch.stop();
         return result;
     }
 }
